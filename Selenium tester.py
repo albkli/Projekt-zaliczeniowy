08af4c9624c6a757f92e76281e503d1b6f842994
Albert K <albertklimowicz@gmail.com>
2024-06-23 16:03:20 +0200

    sortowanie

--- Selenium tester.py
+++ Selenium tester.py
@@ -13,4 +13,31 @@ timesleep= 100
 driver.get('https://www.wakacje.pl/wczasy/?src=fromSearch')
 
 # Wyszukiwanie państwa (dostosuj do rzeczywistej strony)
-search_box = driver.find_element('id', "__next")
+accept = driver.find_element("xpath", "/html/body/div[3]/div/div[2]/div[3]/div/button[2]")
+accept.click()
+driver.implicitly_wait(10)
+
+search_place = driver.find_element("xpath", "//*[@id='__next']/div[2]/div[1]/main/div/div[1]/section/div/div/div/div[1]/div/div/div/input")
+search_place.click()
+driver.implicitly_wait(10)
+
+searchbox = driver.find_element("xpath", "/html/body/div[7]/div/div[1]/div[1]/div/div[1]/div/div/div/div/div[1]/input")
+searchbox.send_keys("Hiszpania")
+driver.implicitly_wait(10)
+
+picked_country = driver.find_element("xpath", "/html/body/div[7]/div/div[1]/div[1]/div/div[2]/div/div[1]/div/div[1]/ul[1]/li[2]/label/div")
+picked_country.click()
+
+go_search = driver.find_element("xpath", "/html/body/div[7]/div/div[1]/div[2]/footer/button")
+go_search.click()
+
+sort_option = driver.find_element("xpath", "//*[@id='__next']/div[2]/div[1]/main/div/div[2]/div[1]/div[1]/div[2]/div/div/div/div[1]/span")
+sort_option.click()
+
+sort_option = driver.find_element("xpath", "//*[@id='__next']/div[2]/div[1]/main/div/div[2]/div[1]/div[1]/div[2]/div/div/div/div[1]/span")
+sort_option.click()
+
+time.sleep(500)
+# search_box = driver.find_element('id', "__next")
+# search_box.send_keys("Francja")
+# search_box.send_keys(Keys.RETURN)
